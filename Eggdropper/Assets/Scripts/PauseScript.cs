a917de20dd44059c2aad247cd0954039c53a800a
Jeff Reiner <JDReiner.IO@Gmail.com>
2015-01-30 20:50:36 -0500

    Couple Edits
    Nothing Too Big, Added Pause Script

---
+++ Eggdropper/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour 
+{	
+	void Update () 
+	{
+     	if (Input.GetKeyDown(KeyCode.P)) 
+     	{
+    		Time.timeScale = 0;
+        }
+
+        //Add Toggleablity
+	}
+}
